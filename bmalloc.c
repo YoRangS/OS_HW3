eb3fd7bdf98d4009f5776a7a93ac67d832ee7a7d
JungEunchong <21900673@handong.ac.kr>
2023-04-29 23:01:29 +0900

    Add comment

--- bmalloc.c
+++ bmalloc.c
@@ -7,40 +7,44 @@ bm_header bm_list_head = {0, 0, 0x0 } ;
 
 void * sibling (void * h)
 {
-	// TODO
+	// returns the header address of the suspected sibling block of h.
 }
 
 int fitting (size_t s) 
 {
-	// TODO
+	// returns the size field value of a fitting block to accommodate s bytes.
+
 }
 
 void * bmalloc (size_t s) 
 {
-	// TODO 
+	// allocates a buffer of s-bytes and returns its starting address.
 	return 0x0 ; // erase this
 }
 
 void bfree (void * p) 
 {
-	// TODO 
+	// free the allocated buffer starting at pointer p.
 }
 
 void * brealloc (void * p, size_t s) 
 {
-	// TODO
+	// resize the allocated memory buffer into s bytes.
+	// As the result of this operation, the data may be immigrated to a different address, as like realloc possibly does.
+
 	return 0x0 ; // erase this 
 }
 
 void bmconfig (bm_option opt) 
 {
-	// TODO
+	// set the space management scheme as BestFit, or FirstFit.
 }
 
 
 void 
 bmprint () 
 {
+	// print out the internal status of the block list to the standard output.
 	bm_header_ptr itr ;
 	int i ;
 
@@ -57,4 +61,5 @@ bmprint ()
 	printf("=================================================\n") ;
 
 	//TODO: print out the stat's.
+
 }
